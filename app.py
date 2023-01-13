bb0f134deeab6dd979599dc73aa62d4e4907cc9e
Jared Caraway <jared.caraway@gmail.com>
2023-01-13 17:37:39 -0600

    Basic setup

---
+++ app.py
@@ -0,0 +1,11 @@
+# accept list of URLs as input
+# the 'validators' package is one possible option
+import validators
+
+# validate input and check for errors
+
+# handle errors, if any
+
+# if it checks out, generate the "meat" of the OPML file
+
+# finally, export the output - an OPML file.
