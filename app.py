207b9c60562255ba63db3a949c90cf532229d5bc
Jared Caraway <jared.caraway@gmail.com>
2023-01-13 17:43:26 -0600

    Add validation package and test it

--- app.py
+++ app.py
@@ -3,6 +3,14 @@
 import validators
 
 # validate input and check for errors
+# here's a bad URL
+url1 = "lksjdflkjsdlkfjslkdfjlksjdf"
+
+# and here's a good URL
+url2 = "http://www.theverge.com/rss/index.xml"
+
+print("Valid URL!" if validators.url(url1) else "Invalid URL!")
+print("Valid URL!" if validators.url(url2) else "Invalid URL!")
 
 # handle errors, if any
 
